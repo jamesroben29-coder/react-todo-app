1ef77de5a85af31051b66c193e0f1a3e555fd7bb
Nyi Nyi <jamesroben29@gmail.com>
2026-09-17 23:25:22 +0800

    created a new line text

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+
+// // https://vite.dev/config/
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/react-todo-app/',
+// })
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-
 export default defineConfig({
   plugins: [react()],
-  base: '/react-todo-app/',
 })
